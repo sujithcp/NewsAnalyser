1c26af7e6488ea6fe0eea2cc2745c78a13a9924e
SharanyaKV <sharanyakv96@gmail.com>
2016-05-06 10:03:24 +0530

    Code for finding tf_idf score of terms

---
+++ fetchData.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def fetchNewsFromDb(start_date=None, end_date=None):
+    connection = sqlite3.connect('data/news_data.db')
+    cursor = connection.cursor()
+    if not start_date or not end_date:
+        data = cursor.execute('''select * from News''').fetchall()
+    else:
+        data = cursor.execute('''select * from News where ? <= date and date <= ?  order by date asc,time asc''', (start_date, end_date,)).fetchall()
+    return data
