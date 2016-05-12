7e0795bb29e3760b997fabc26de07d778640e090
SharanyaKV <sharanyakv96@gmail.com>
2016-05-12 14:18:17 +0530

    detect latest trending events

--- event_detection.py
+++ event_detection.py
@@ -15,7 +15,7 @@ def detectEvent(start_date , end_date):
     print('------')
     print('------')
     print('------')
-    data1=cursor.execute('select temp.term from TempTermScore as temp, TermScore as ts where temp.term = ts.term and temp.score >= 4*ts.score').fetchall()
+    data1=cursor.execute('select temp.term from TempTermScore as temp, TermScore as ts where temp.term = ts.term and temp.score >= 6*ts.score').fetchall()
     data2=cursor.execute('select term from TempTermScore where term not in (select term from TermScore) and score > 20').fetchall()
     #data = cursor.execute('''select temp.term from TempTermScore as temp,TermScore as ts where (temp.term=ts.term and temp.score>=(2*ts.score)) or temp.term not in (select distinct term from TermScore)''').fetchall()
 
@@ -38,4 +38,4 @@ def detectEvent(start_date , end_date):
         print(' '+term)
 
 
-detectEvent("2016-05-05","2016-05-10")
+detectEvent("2016-05-05","2016-05-12")
