7eee0c5c47b96e27c38811787e1abbf1df8df142
SharanyaKV <sharanyakv96@gmail.com>
2016-05-13 10:30:13 +0530

    Detects event in given window

--- event_detection.py
+++ event_detection.py
@@ -1,44 +1,27 @@
-import sqlite3
+import datetime
 
-from StoreTermScore import storeTermScore
-from event_in_week import event_in_week
-
-
-def detectEvent(start_date , end_date):
-    connection = sqlite3.connect('data/news_data.db')
-    cursor = connection.cursor()
-
-    cursor.execute('''delete from TempTermScore''')
-    connection.commit()
-    print('------- ')
-    storeTermScore(start_date ,end_date ,"TempTermScore")
-    print('------')
-    print('------')
-    print('------')
-    data1=cursor.execute('select temp.term from TempTermScore as temp, TermScore as ts where temp.term = ts.term and temp.score >= 6*ts.score').fetchall()
-    data2=cursor.execute('select term from TempTermScore where term not in (select term from TermScore) and score > 20').fetchall()
-    #data = cursor.execute('''select temp.term from TempTermScore as temp,TermScore as ts where (temp.term=ts.term and temp.score>=(2*ts.score)) or temp.term not in (select distinct term from TermScore)''').fetchall()
-
-    data1=data1[:10]
-    data1.extend( data2[:10])
-    data1 = [i[0] for i in data1]
-
-    event_in_week(start_date , end_date )
-
-    data2 = cursor.execute('select word from final_tfidf order by tfidf desc limit 20').fetchall()
-    data2 = [i[0] for i in data2]
-    print(data1)
-    print('----------x----------')
-    print(data2)
-    print('----------x----------')
-    event = [term for term in data1 if term in data2 ]
-
-    print('<<< New Trending Event >>>')
-    if event==[]:
-        print('No new trending event')
-    else:
-        for term in event:
-            print(' '+term)
+from event_in_window import detectEvent
 
 
-detectEvent("2016-05-01","2016-05-12")
+def final(start_date, end_date,window_size):
+    window_size-=1
+
+    current_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    final_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    events=[]
+    while current_date <= final_date:
+        next_date = (current_date + datetime.timedelta(days=window_size))
+
+        events.append(detectEvent(current_date.strftime('%Y-%m-%d'), next_date.strftime('%Y-%m-%d')))
+
+        current_date = next_date + datetime.timedelta(days=1)
+
+    for window in events:
+        print('\n\n<<< Trending Events : ', window[0], '-', window [1])
+        if window [2] ==[]:
+            print('  No new trending events in news')
+        else:
+            for i in window [2]:
+                print(' ',i)
+
+final("2016-05-01","2016-05-12",5)
