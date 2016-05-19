627fe53ec2e98d9d36144816fd71bf6d136cc556
SharanyaKV <sharanyakv96@gmail.com>
2016-05-19 10:23:05 +0530

    Plots graph ( Date vs No. of news )

--- event_prev/plot_graph.py
+++ event_prev/plot_graph.py
@@ -2,7 +2,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.dates import  date2num
 import sqlite3
-import datetime
+import datetime as DT
 
 def plotDB():
     connection=sqlite3.connect('../data/news_data.db')
@@ -12,15 +12,36 @@ def plotDB():
 
     data=[i for i in data if i[0] != 'NULL']
 
-    date=[]
-    for i in data:
-        date.append(datetime.datetime.strptime(i[0], '%Y-%m-%d'))
+    data = [(DT.datetime.strptime(i[0], "%Y-%m-%d"), i[1]) for i in data]
 
-    date= [date2num(i)  for i in date ]
-    count=[i[1] for i in data]
 
-    plt.plot(date, count, 'b')
-    plt.axis()
+    x = [date2num(date) for (date, value) in data]
+    y = [value for (date, value) in data]
+
+    fig = plt.figure()
+
+    graph = fig.add_subplot(111)
+
+# Plot the data as a red line with round markers
+    graph.plot(x, y, 'b-o')
+
+# Set the xtick locations to correspond to just the dates you entered.
+    graph.set_xticks(x)
+    datacopy=[]
+    n=len(data)
+    for i in  range(n):
+        if i%14==0:
+            datacopy .append(data[i][0].strftime("%Y-%m-%d") )
+        elif i==n-1:
+            datacopy.append(data[i][0].strftime("%Y-%m-%d"))
+        else:
+            datacopy.append(" ")
+# Set the xtick labels to correspond to just the dates you entered.
+
+    #graph.set_xticklabels( [date.strftime("%Y-%m-%d") for (date, value) in data])
+
+    graph.set_xticklabels([date for date in datacopy ])
+
     plt.show()
 
 plotDB()
